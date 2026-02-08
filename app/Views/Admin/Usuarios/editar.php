8c78750952daa7178f6838f6fe81144a3f15e558
Juliodev10 <jcking0@hotmail.com>
2026-02-08 15:24:04 -0300

    13-atualizar

---
+++ app/Views/Admin/Usuarios/editar.php
@@ -0,0 +1,45 @@
+<?= $this->extend('Admin/layout/principal'); ?>
+
+
+<?= $this->section('titulo'); ?>
+<?= $titulo; ?>
+<?= $this->endSection() ?>
+
+<?= $this->section('estilos'); ?>
+
+<style>
+    .ui-autocomplete {
+        z-index: 2000;
+    }
+</style>
+
+<?= $this->endSection() ?>
+
+<?= $this->section('conteudo'); ?>
+
+<div class="row">
+
+    <div class="col-lg-12 grid-margin stretch-card">
+        <div class="card">
+            <div class="card-header bg-primary pb-0 pt-4">
+                <h4 class="card-title text-white">
+                    <?= esc($titulo); ?>
+                </h4>
+            </div>
+            <div class="card-body">
+                <?php echo form_open("admin/usuarios/atualizar/$usuario->id"); ?>
+                <?= csrf_field() ?>
+                <?php echo $this->include('Admin/Usuarios/form'); ?>
+                <?php echo form_close(); ?>
+            </div>
+        </div>
+    </div>
+
+    <?= $this->endSection() ?>
+
+    <!-- Aqui enviamos para o template principal os scripts -->
+    <?= $this->section('scripts'); ?>
+    <script src="<?php echo site_url('/admin/vendors/mask/jquery.mask.min.js'); ?>"></script>
+    <script src="<?php echo site_url('/admin/vendors/mask/app.js'); ?>"></script>
+
+    <?= $this->endSection() ?>
